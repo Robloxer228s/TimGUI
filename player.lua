43d1de4e57ff505fc09f6e3c7f82ba22e4de56ef
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-02-13 19:33:30 +0300

    Update player.lua

--- player.lua
+++ player.lua
@@ -6,6 +6,27 @@ local ESPTC = _G.ACBF("Esptc", "Use Team Color(esp-main)", "ESP", 2)
 
 ESPV.Changed:Connect(function() 
 if not espLoad then
+for k,player in pairs(game.Players:GetChildren()) do
+if not (player == game.Players.LocalPlayer) then
+player.CharacterAdded:Connect(function(char) 
+if ESPV.Value then
+local ESP = Instance.new("Highlight")
+ESP.Parent = char
+ESP.Name = "NotEsp"
+ESP.Adornee = char
+ESP.Archivable = true
+ESP.Enabled = true
+ESP.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+ESP.FillColor = Color3.new(1,1,1)
+if ESPTC.Value then
+ESP.FillColor = player.TeamColor.Color
+end
+ESP.FillTransparency = 0.5
+ESP.OutlineColor = ESP.FillColor
+ESP.OutlineTransparency = 0
+end
+end) 
+end
 game.Players.PlayerAdded:Connect(function(player)
 player.CharacterAdded:Connect(function(char) 
 if ESPV.Value then
@@ -29,7 +50,7 @@ end)
 end
 espLoad = true
 for k,player in pairs(game.Players:GetChildren()) do
-if ESPV.Value then
+if ESPV.Value and not (player == game.Players.LocalPlayer) then
 local ESP = Instance.new("Highlight")
 ESP.Parent = player.Character
 ESP.Name = "NotEsp"
@@ -45,7 +66,10 @@ ESP.FillTransparency = 0.5
 ESP.OutlineColor = ESP.FillColor
 ESP.OutlineTransparency = 0
 else
-player.Character.NotEsp:Destroy()
+local Esp = player.Character:FindFirstChildForClass("Highlight") 
+if Esp then
+Esp:Destroy()
+end
 end
 end
 end)
