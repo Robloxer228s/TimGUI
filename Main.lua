e51ec3039bd27972a0bd7126c7649acc061862b9
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-02-13 07:56:17 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -59,8 +59,11 @@ tween:Play()
 end
 end) 
 
+local ABF
+local ATBF
+local ACBF
+local AGF
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGui/main/service.lua"))()
-
 AGF(FA)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGui/main/player.lua"))()
 local gameURL = game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGUI/main/".. game.PlaceId ..".lua")()
