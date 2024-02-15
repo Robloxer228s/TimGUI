5307dea6dcf2205da4da399dfcca5cec81f23473
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-02-15 10:15:14 +0300

    Create 662417684.lua

---
+++ 662417684.lua
@@ -0,0 +1,38 @@
+_G.AGF("LuckyBlocks") 
+local count = _G.ATBF("count", "Quantity:", "LuckyBlocks", 1) 
+count.Text = 1
+local zero = _G.ABF("zero", "LuckyBlock", "LuckyBlocks", 2) 
+local one = _G.ABF("one", "SuperBlock", "LuckyBlocks", 3) 
+local two = _G.ABF("two", "DiamondBlock", "LuckyBlocks", 4) 
+local three = _G.ABF("three", "RainbowBlock", "LuckyBlocks", 5) 
+local four = _G.ABF("four", " GalaxyBlock", "LuckyBlocks", 6) 
+
+zero.Activated:Connect(function() 
+for v = 0, count.Text, 1 do
+game.ReplicatedStorage.SpawnLuckyBlock:FireServer() 
+end
+end) 
+
+one.Activated:Connect(function() 
+for v = 0, count.Text, 1 do
+game.ReplicatedStorage.SpawnSuperBlock:FireServer() 
+end
+end) 
+
+two.Activated:Connect(function() 
+for v = 0, count.Text, 1 do
+game.ReplicatedStorage.SpawnDiamondBlock:FireServer() 
+end
+end) 
+
+three.Activated:Connect(function() 
+for v = 0, count.Text, 1 do
+game.ReplicatedStorage.SpawnRainbowBlock:FireServer() 
+end
+end) 
+
+four.Activated:Connect(function() 
+for v = 0, count.Text, 1 do
+game.ReplicatedStorage.SpawnGalaxyBlock:FireServer() 
+end
+end) 
