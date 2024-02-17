ac89b36d215cd44a75237837be5480330c35bf94
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-02-17 13:39:15 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,8 +1,11 @@
 --[[
 Please, use this script(for english language):
 
+
 _G.eng = true
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGUI/main/Main.lua"))()
+
+
 ]]
 local FoldersT = {}
 local FA = "TP to player"
