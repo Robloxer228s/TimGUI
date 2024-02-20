4325901c4b8a95c7e4b4e142241ab1ef29e778bf
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-02-20 14:34:33 +0300

    Create 4669328522.lua

---
+++ 4669328522.lua
@@ -0,0 +1,77 @@
+local ESPM = _G.ACBF("ESPM", "ESP Monsters", "ESP", 1, "ESP на монстров") 
+ESPM.Changed:Connect(function()
+for k, chr in pairs(game.Workspace.Maps:GetChildren()) do
+chr = chr:FindFirstChild("Killers") 
+if chr then
+for k, char in pairs(chr:GetChildren()) do
+if ESPM.Value and char.ClassName == "Model" then
+local ESP = Instance.new("Highlight")
+ESP.Parent = char
+ESP.Name = "NotEsp"
+ESP.Adornee = char
+ESP.Archivable = true
+ESP.Enabled = true
+ESP.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+ESP.FillColor = Color3.new(1,0,0)
+ESP.FillTransparency = 0.5
+ESP.OutlineColor = ESP.FillColor
+ESP.OutlineTransparency = 0
+else
+if char:FindFirstChild("NotEsp") and char.ClassName == "Model" then
+char.NotEsp:Destroy()
+end
+end
+end
+end
+end
+end) 
+
+local ESPI = _G.ACBF("ESPI", "ESP Items", "ESP", 2, "ESP на предметы") 
+ESPI.Changed:Connect(function()
+for k, chr in pairs(game.Workspace.Maps:GetChildren()) do
+for k, char in pairs(chr:GetChildren()) do
+if char.Name == "Collections" then
+for k, charr in pairs(char:GetChildren()) do
+if charr.Name == "CollectionProvider" then
+if ESPI.Value and charr.ClassName == "Model" then
+local ESP = Instance.new("Highlight")
+ESP.Parent = charr
+ESP.Name = "NotEsp"
+ESP.Adornee = charr
+ESP.Archivable = true
+ESP.Enabled = true
+ESP.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+ESP.FillColor = Color3.new(0,1,1)
+ESP.FillTransparency = 0.5
+ESP.OutlineColor = ESP.FillColor
+ESP.OutlineTransparency = 0
+elseif char.ClassName == "Model" then
+if char:FindFirstChild("NotEsp") then
+char.NotEsp:Destroy()
+end
+end
+end
+end
+end
+if char.Name == "CollectionProvider" then
+if ESPI.Value and char.ClassName == "Model" then
+local ESP = Instance.new("Highlight")
+ESP.Parent = char
+ESP.Name = "NotEsp"
+ESP.Adornee = char
+ESP.Archivable = true
+ESP.Enabled = true
+ESP.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+ESP.FillColor = Color3.new(0,1,1)
+ESP.FillTransparency = 0.5
+ESP.OutlineColor = ESP.FillColor
+ESP.OutlineTransparency = 0
+elseif char.ClassName == "Model" then
+if char:FindFirstChild("NotEsp") then
+char.NotEsp:Destroy()
+end
+end
+end
+end
+end
+end) 
