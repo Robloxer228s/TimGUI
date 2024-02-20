4581d2f7fb977965dfe9c26ce859c36c7703b26c
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-02-20 10:53:20 +0300

    Create 1645606267.lua

---
+++ 1645606267.lua
@@ -0,0 +1,70 @@
+local ESPV = _G.ACBF("ESPV", "ESP all", "ESP", 3, "ESP все") 
+local ESPF = _G.ACBF("ESPF", "ESP froggy", "ESP", 4, "ESP к лягушке") 
+
+local function CESP(char)
+local ESP = Instance.new("Highlight")
+ESP.Parent = char
+ESP.Name = "NotEsp"
+ESP.Adornee = char
+ESP.Archivable = true
+ESP.Enabled = true
+ESP.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+ESP.FillColor = Color3.new(1,1,1)
+local dest = false
+if char.Name == "Frogge" then
+if ESPF.Value then
+ESP.FillColor = Color3.new(0, 1, 0) 
+else
+dest = true
+end
+end
+ESP.FillTransparency = 0.5
+ESP.OutlineColor = ESP.FillColor
+ESP.OutlineTransparency = 0
+if dest then
+ESP:Destroy() 
+end
+end
+
+for k,player in pairs(game.Players:GetChildren()) do
+if not (player == game.Players.LocalPlayer) then
+player.CharacterAdded:Connect(function(char) 
+if ESPV.Value then
+CESP(char) 
+elseif char.Name == "Frogge" and ESPF.Value then
+CESP(char) 
+end
+end)
+end
+end
+game.Players.PlayerAdded:Connect(function(player)
+player.CharacterAdded:Connect(function(char) 
+if ESPV.Value then
+CESP(char) 
+elseif char.Name == "Frogge" and ESPF.Value then
+CESP(char) 
+end
+end) 
+end) 
+
+ESPV.Changed:Connect(function() 
+for k,player in pairs(game.Players:GetChildren()) do
+local char = player.Character
+if ESPV.Value and not (player == game.Players.LocalPlayer) then
+CESP(char) 
+elseif not ESPV.Value then
+CESP(char) 
+end
+end
+end) 
+
+ESPF.Changed:Connect(function() 
+for k,player in pairs(game.Players:GetChildren()) do
+local char = player.Character
+if char.Name == "Frogge" and ESPF.Value then
+CESP(char) 
+elseif char.Name == "Frogge" and char:FindFirstChild("NotEsp") then
+char.NotEsp:Destroy() 
+end
+end
+end) 
