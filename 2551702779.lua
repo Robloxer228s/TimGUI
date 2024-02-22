f2c2d208ff636f732de54157f17989ba112a8896
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-02-22 19:24:02 +0300

    Update 2551702779.lua

--- 2551702779.lua
+++ 2551702779.lua
@@ -7,6 +7,7 @@ local kaz = _G.ABF("kaz", "Kill All Zombies", "Zombie lab", 5,"Убить все
 local ZN = _G.ATBF("ZN", "Zombie:", "Zombie lab", 6,"Зомби:") 
 local kz = _G.ABF("kz", "Kill Zombie", "Zombie lab", 7,"Убить зомби")
 local knz = _G.ABF("knz", "Kill NPC Zombies", "Zombie lab", 8,"Убить НПС зомби")
+local aknz = _G.ACBF("aknz", "Kill NPC Zombies(auto)", "Zombie lab", 8,"Убить НПС зомби(авто)")
 virus.Activated:Connect(function() 
 game.ReplicatedStorage.Events.GiveVirus:FireServer()
 end) 
@@ -82,3 +83,18 @@ pistol().GunScript_Server.InflictTarget:FireServer(unpack(args))
 end)
 end
 end) 
+
+while true do 
+wait(1) 
+if aknz then
+local args = {
+    [1] = v:WaitForChild("Humanoid"),
+    [2] = v:WaitForChild("HumanoidRootPart"),
+    [3] = math.huge,
+}
+pcall(function()
+pistol().GunScript_Server.InflictTarget:FireServer(unpack(args))
+end)
+end
+end
+end 
