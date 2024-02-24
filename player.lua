7785534fef0dc7811bd6163b3df521e28323ca5a
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-02-24 23:06:38 +0300

    Update player.lua

--- player.lua
+++ player.lua
@@ -10,6 +10,28 @@ JB.Activated:Connect(function()
 game.Players.LocalPlayer.Character.Humanoid.JumpPower = JT.Text
 end)
 
+_G.ABF("GM", "GodMode", "Main", 10, "Бесмертие").Activated:Connect(function()
+local speaker = game.Players.LocalPlayer
+local Cam = workspace.CurrentCamera
+local Pos, Char = Cam.CFrame, speaker.Character
+local Human = Char and Char.FindFirstChildWhichIsA(Char, "Humanoid")
+local nHuman = Human.Clone(Human)
+nHuman.Parent, speaker.Character = Char, nil
+nHuman.SetStateEnabled(nHuman, 15, false)
+nHuman.SetStateEnabled(nHuman, 1, false)
+nHuman.SetStateEnabled(nHuman, 0, false)
+nHuman.BreakJointsOnDeath, Human = true, Human.Destroy(Human)
+speaker.Character, Cam.CameraSubject, Cam.CFrame = Char, nHuman, wait() and Pos
+nHuman.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.None
+local Script = Char.FindFirstChild(Char, "Animate")
+if Script then
+Script.Disabled = true
+wait()
+Script.Disabled = false
+end
+nHuman.Health = nHuman.MaxHealth
+end)
+
 local PP = _G.ATBF("SpinV","SpinPower:","Main",5,"Скорость кружения") 
 local PB = _G.ACBF("SpinB", "Spining", "Main", 6,"Крутиться") 
 PB.Changed:Connect(function()
