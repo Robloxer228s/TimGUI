2f28b5ab7806f6e3ca965b9b72ec73227dbd6593
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-02-24 14:52:59 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -252,8 +252,8 @@ if autotp.Value then
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = atpp.Character.HumanoidRootPart.CFrame
 end
 end) 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGui/main/WayPoints.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGui/main/player.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGui/main/Map.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGui/main/player.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGui/main/WayPoints.lua"))()
 print(game.GameId) 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGUI/main/".. game.GameId ..".lua"))()
