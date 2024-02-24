71915728fc62e54750a5c312cc12dff921291493
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-02-24 23:27:26 +0300

    Update player.lua

--- player.lua
+++ player.lua
@@ -1,4 +1,25 @@
 _G.AGF("Main","Стандартные")
+_G.ABF("GM", "GodMode", "Main", 10, "Бесмертие").Activated:Connect(function()
+local speaker = game.Players.LocalPlayer
+local Cam = workspace.CurrentCamera
+local Pos, Char = Cam.CFrame, speaker.Character
+local Human = Char and Char.FindFirstChildWhichIsA(Char, "Humanoid")
+local nHuman = Human.Clone(Human)
+nHuman.Parent, speaker.Character = Char, nil
+nHuman.SetStateEnabled(nHuman, 15, false)
+nHuman.SetStateEnabled(nHuman, 1, false)
+nHuman.SetStateEnabled(nHuman, 0, false)
+nHuman.BreakJointsOnDeath, Human = true, Human.Destroy(Human)
+speaker.Character, Cam.CameraSubject, Cam.CFrame = Char, nHuman, wait() and Pos
+nHuman.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.None
+local Script = Char.FindFirstChild(Char, "Animate")
+if Script then
+Script.Disabled = true
+wait()
+Script.Disabled = false
+end
+nHuman.Health = nHuman.MaxHealth
+end)
 local WST = _G.ATBF("WalkspeedV","WalkSpeed:","Main",1,"Скорость ходьбы:") 
 local WSB = _G.ABF("WalkspeedB", "Set walkSpeed", "Main", 2,"Установить скорость")
 WSB.Activated:Connect(function()
