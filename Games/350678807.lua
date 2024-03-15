47c07458547079e5bbd56239ff4186589879134b
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-03-15 09:45:30 +0300

    Update 350678807.lua

--- Games/350678807.lua
+++ Games/350678807.lua
@@ -1,6 +1,4 @@
 _G.AGF("Door") 
-local BA = _G.ABF("BA", "Become amogus", "Door", 1) 
-
-BA.Activated:Connect(function() 
+local BA = _G.ABF("BA", "Become amogus", "Door", 1, function() 
 game.ReplicatedStorage.Amogus:FireServer() 
 end) 
