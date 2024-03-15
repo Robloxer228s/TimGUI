d06606f60ba2d067ada466a23afcbb1e27862d48
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-03-15 17:28:05 +0300

    Update Main.lua

--- TimControl/Main.lua
+++ TimControl/Main.lua
@@ -24,7 +24,7 @@ return 0
 end
 
 local code = 0
-_G.ACBF("TC", "TimConnect", "Player", 14, "TimConnect", function(TC)
+_G.ACBF("TC", "TimConnect", "Player", 13, "TimConnect", function(TC)
 if TC.Value then 
 TC.Parent.TextButton.Text = "Connecting"
 code = Connect()
