f4b5122ce8e9139d8ac48b50ebea5ac88a6b454a
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-03-30 12:35:56 +0300

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,12 @@
+--[[
+Please, use this script(for english language):
+
+
+_G.eng = true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGUI/main/Main.lua"))()
+
+
+]]
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGUI/main/TimGui.lua"))()
