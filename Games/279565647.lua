76296e68860ff5959b6cd195b19e88bce4e1eac6
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-03-30 12:53:48 +0300

    Update 279565647.lua

--- Games/279565647.lua
+++ Games/279565647.lua
@@ -1,5 +1,5 @@
-_G.AGF("LuckyBlocks") 
-local count = _G.ATBF("count", "Quantity:", "LuckyBlocks", 1,"Количество:") 
+_G.TimGui.Add.G("LuckyBlocks") 
+local count = _G.TimGui.Add.TB("count", "Quantity:", "LuckyBlocks", 1,"Количество:") 
 count.Text = 1
 _G.ABF("zero", "LuckyBlock", "LuckyBlocks", 2 ,nil , function() 
 for v = 0, count.Text, 1 do
@@ -7,25 +7,25 @@ game.ReplicatedStorage.SpawnLuckyBlock:FireServer()
 end
 end) 
 
-_G.ABF("one", "SuperBlock", "LuckyBlocks", 3, nil, function() 
+_G.TimGui.Add.B("one", "SuperBlock", "LuckyBlocks", 3, nil, function() 
 for v = 0, count.Text, 1 do
 game.ReplicatedStorage.SpawnSuperBlock:FireServer() 
 end
 end) 
 
-_G.ABF("two", "DiamondBlock", "LuckyBlocks", 4, nil, function() 
+_G.TimGui.Add.B("two", "DiamondBlock", "LuckyBlocks", 4, nil, function() 
 for v = 0, count.Text, 1 do
 game.ReplicatedStorage.SpawnDiamondBlock:FireServer() 
 end
 end) 
 
-_G.ABF("three", "RainbowBlock", "LuckyBlocks", 5, nil, function() 
+_G.TimGui.Add.B("three", "RainbowBlock", "LuckyBlocks", 5, nil, function() 
 for v = 0, count.Text, 1 do
 game.ReplicatedStorage.SpawnRainbowBlock:FireServer() 
 end
 end) 
 
-_G.ABF("four", " GalaxyBlock", "LuckyBlocks", 6, nil, function() 
+_G.TimGui.Add.B("four", " GalaxyBlock", "LuckyBlocks", 6, nil, function() 
 for v = 0, count.Text, 1 do
 game.ReplicatedStorage.SpawnGalaxyBlock:FireServer() 
 end
