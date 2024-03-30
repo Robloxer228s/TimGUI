9e079dd0d62bf57bc6e57212e67755b2d63d310e
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-03-30 17:38:12 +0300

    Update TimGui.lua

--- TimGui.lua
+++ TimGui.lua
@@ -16,9 +16,9 @@ _G.TimGui = {}
 _G.TimGui.Add = {}
 _G.TimGui.TimControlSet = function(GN, mode, data)
 if mode == "CB" then
-TOCB[GN].Value = data
+TOCB[GN].Value.Value = data
 elseif mode == "TB" then
-TOCB[GN].Text = data
+TOCB[GN].Text.Text = data
 elseif mode == "B" then
 local temp = BFuncs[GN]
 pcall(temp)
