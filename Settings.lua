c5f078f58e2926dbe38b67502e1955fb4c5c1e99
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-04-04 11:02:32 +0300

    Create Settings.lua

---
+++ Settings.lua
@@ -0,0 +1,13 @@
+_G.TimGui.Add.CB("rus", "Русский язык", "Settings", 1, "English language", function(rusV)
+_G.TimGui.ru = rusV
+for k, v in pairs(_G.TimGui.Path.Buttons:GetChildren()) do
+local tmp = v.Text.Text
+v.Text.Text = v.Text.Value.Value
+v.Text.Value.Value = tmp
+end
+for k, v in pairs(_G.TimGui.Path.Groups:GetChildren()) do
+local tmp = v.Text.Text
+v.Text.Text = v.Text.Value.Value
+v.Text.Value.Value = tmp
+end
+end)
