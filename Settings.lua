8bcfcc5c3414b0a465a96e11c48a8d09f55b70f1
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-04-05 10:23:34 +0300

    Update Settings.lua

--- Settings.lua
+++ Settings.lua
@@ -11,3 +11,28 @@ v.Text.Text = v.Text.Value.Value
 v.Text.Value.Value = tmp
 end
 end)
+_G.TimGui.Add.CB("x2", "X2 interface", "Settings", 2, "2 кнопки", function(interface)
+if interface.Value then
+_G.TimGui.Path.Main.Size = UDim2.new(0, 700, 1, 0)
+_G.TimGui.Path.Main.Position = UDim2.new(1, -700, 0, 0)
+else
+_G.TimGui.Path.Main.Size = UDim2.new(0, 400, 1, 0)
+_G.TimGui.Path.Main.Position = UDim2.new(1, -400, 0, 0)
+end
+
+for k,v in pairs(_G.TimGui.Path.Buttons:GetChildren()) do
+_G.TimGui.XTwo = interface.Value
+local pos = v.pos.Value
+if interface.Value then
+pos = pos / 2
+local poss = math.ceil(pos) 
+if poss == pos then
+v.Position = UDim2.new(1, -300, 0, 50 * (poss - 1))
+else
+v.Position = UDim2.new(0, 0, 0, 50 * (poss - 1))
+end
+else
+v.Position = UDim2.new(0, 0, 0, 50 * (pos - 1))
+end
+end
+end)
