43ceca3c6a01dfa22f6aea9eae548f3aeefaaee2
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-04-05 20:10:45 +0300

    Update Configs.lua

--- Configs.lua
+++ Configs.lua
@@ -1,5 +1,8 @@
 local getF = function(name)
-local post = {"operation": "fileget", "id": "Yzk2MzY4NzI0NmNhOTIwNDI4NGMxNDUyZmRhYmYyM2RlZGYwOGRiM2U5ODhmN2Q2ZGI0MzI1MjllN2IzMDJhMQ==", "file": name}
+local post = {"operation": "fileget",
+    "id": "Yzk2MzY4NzI0NmNhOTIwNDI4NGMxNDUyZmRhYmYyM2RlZGYwOGRiM2U5ODhmN2Q2ZGI0MzI1MjllN2IzMDJhMQ==",
+    "file": name
+}
 local response = game:HttpPost("http://bagirovict.temp.swtest.ru", game:GetService("HttpService"):DecodeJSON(post))
 response = string.gsub(response, "%.q_%.", '"')
 response = string.gsub(response, "%.ns_%.", '\n')
