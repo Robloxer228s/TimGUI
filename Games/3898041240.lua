40f97633c99b2152a55518e32258c9f9c199de5c
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-04-07 16:32:42 +0300

    Create 3898041240.lua

---
+++ Games/3898041240.lua
@@ -0,0 +1,25 @@
+_G.TimGui.Add.G("Leader", "Лидер") 
+local leader = function()
+for k, v in pairs(game.Players:GetChildren()) do
+local hight = v.Character:FindFirstChild("LeaderHighlight")
+if hight then
+return hight.Parent
+end
+end
+end
+
+
+_G.TimGui.Add.B("Touch", "TP to leader", "Leader", 1, "ТП к лидера", function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = leader().HumanoidRootPart.CFrame
+end)
+
+local ATP =  _G.TimGui.Add.CB("AutoTP", "AutoTP to leader", "Leader", 2, "АвтоТП к лидера")
+
+while true do
+wait(0.05) 
+pcall(function()
+if ATP.Value then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = leader().HumanoidRootPart.CFrame
+end
+end)
+end
