73bcb60ef1aa9652873be0ba5f66cebc425d4cf6
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-04-07 12:52:50 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,10 +1,7 @@
 --[[
-Please, use this script(for english language):
+Please, use this script(for updates):
 
-
-_G.eng = true
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGUI/main/Main.lua"))()
-
 
 ]]
 
