26517f7eb3589fe6e4c93e074a2efc43db446fa8
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-04-09 15:21:10 +0300

    Update 5166944221.lua

--- Games/5166944221.lua
+++ Games/5166944221.lua
@@ -32,12 +32,14 @@ rad = 0
 end
 end
 
-game.Players.LocalPlayer.PlayerGui.UI.HUD.HolderBottom.GeneralNotifications.IntermissionFrame.DescriptionLabel.Changed:Connect(function()
-if game.Players.LocalPlayer.PlayerGui.UI.HUD.HolderBottom.GeneralNotifications.IntermissionFrame.Visible and AFK.Value then
+
+local InterFrame = game.Players.LocalPlayer.PlayerGui.HUD.HolderBottom.IntermissionFrame
+InterFrame.DescriptionLabel.Changed:Connect(function()
+if InterFrame.Visible and AFK.Value then
 if _G.debug then
 print(game.Players.LocalPlayer.PlayerGui.UI.HUD.HolderBottom.GeneralNotifications.IntermissionFrame.DescriptionLabel.Text)
 end
-if game.Players.LocalPlayer.PlayerGui.UI.HUD.HolderBottom.GeneralNotifications.IntermissionFrame.DescriptionLabel.Text == "INTERMISSION 5" then
+if InterFrame.DescriptionLabel.Text == "INTERMISSION 5" then
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace["New Lobby"].ReadyArea.ReadyZone.CFrame
 end
 end
