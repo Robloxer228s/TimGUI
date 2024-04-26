cddc305e004de9d8b6c7ecb2aeb8e63627c11183
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-04-26 20:41:44 +0300

    Update Configs.lua

--- Configs.lua
+++ Configs.lua
@@ -7,7 +7,7 @@ post.file = name
 local response = game:HttpPostAsync("http://bagirovict.temp.swtest.ru", post)
 response = string.gsub(response, "%.q_%.", '"')
 response = string.gsub(response, "%.ns_%.", '\n')
---print(response) 
+print(response) 
 response = game:GetService("HttpService"):JSONDecode(response)
 if response[game.Players.LocalPlayer.Name] == nil then
 response[game.Players.LocalPlayer.Name] = {}
@@ -23,7 +23,7 @@ post.file = name
 local response = game:HttpPost("http://bagirovict.temp.swtest.ru", post)
 response = string.gsub(response, "%.q_%.", '"')
 response = string.gsub(response, "%.ns_%.", '\n')
---print(response) 
+print(response) 
 local tab = game:GetService("HttpService"):JSONDecode(response)
 tab[game.Players.LocalPlayer.Name] = data
 local post = {}
