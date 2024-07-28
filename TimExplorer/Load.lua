c707cdcdd748aec99bdc1a37a06b9a2b0bded6d0
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-07-28 22:06:10 +0300

    Update Load.lua

--- TimExplorer/Load.lua
+++ TimExplorer/Load.lua
@@ -9,24 +9,24 @@ _G.TimGui.Add.CB("TE","TimExplorer","Map",7,"TimExplorer",function(interface)
       MainFrame.Position = UDim2.new(0.8,0,0,0)
       _G.TimGui.Path.Main.Position = UDim2.new(0.8, -400, 0, 0)
       if _G.TimGui.XTwo then
-        goal.Position = UDim2.new(0.8, -700, 0, 0)
+        _G.TimGui.Path.Main.Position = UDim2.new(0.8, -700, 0, 0)
       end
       if not _G.TimGui.Opened then
         _G.TimGui.Path.Main.Position = UDim2.new(0.8, -400, 1, -25)
         if _G.TimGui.XTwo then
-        goal.Position = UDim2.new(0.8, -700, 1, -25)
+        _G.TimGui.Path.Main.Position = UDim2.new(0.8, -700, 1, -25)
       end
       end
     else
       MainFrame.Position = UDim2.new(1,0,0,0)
       _G.TimGui.Path.Main.Position = UDim2.new(1, -400, 0, 0)
       if _G.TimGui.XTwo then
-        goal.Position = UDim2.new(1, -700, 0, 0)
+        _G.TimGui.Path.Main.Position = UDim2.new(1, -700, 0, 0)
       end
       if not _G.TimGui.Opened then
         _G.TimGui.Path.Main.Position = UDim2.new(1, -400, 1, -25)
         if _G.TimGui.XTwo then
-          goal.Position = UDim2.new(1, -700, 1, -25)
+          _G.TimGui.Path.Main.Position = UDim2.new(1, -700, 1, -25)
         end
       end
     end
