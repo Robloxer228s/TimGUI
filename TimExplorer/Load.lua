42afd07edb16d7e6765b9e09bc808e96f580b662
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-07-28 21:38:26 +0300

    Update Load.lua

--- TimExplorer/Load.lua
+++ TimExplorer/Load.lua
@@ -3,9 +3,9 @@ local MainFrame = game.CoreGui.TimEXPLORER:FindFirstChildOfClass("Frame")
 MainFrame.Position = UDim2.new(1,0,0,0)
 
 _G.TimGui.Add.CB("TE","TimExplorer","Map",7,"TimExplorer",function(interface)
-    MainFrame.Position = UDim2.new(0.8,0,0,0)
     _G.TimGui.TEOpen = interface.Value
     if interface.Value then
+      MainFrame.Position = UDim2.new(0.8,0,0,0)
       _G.TimGui.Path.Main.Position = UDim2.new(0.8, -400, 0, 0)
       if _G.TimGui.XTwo then
         goal.Position = UDim2.new(0.8, -700, 0, 0)
@@ -17,6 +17,7 @@ _G.TimGui.Add.CB("TE","TimExplorer","Map",7,"TimExplorer",function(interface)
       end
       end
     else
+      MainFrame.Position = UDim2.new(1,0,0,0)
       _G.TimGui.Path.Main.Position = UDim2.new(1, -400, 0, 0)
       if _G.TimGui.XTwo then
         goal.Position = UDim2.new(1, -700, 0, 0)
