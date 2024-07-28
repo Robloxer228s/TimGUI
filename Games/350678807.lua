c1fb95924cdf014371f86476f8546e6d31e377ab
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-07-28 22:52:03 +0300

    Update 350678807.lua

--- Games/350678807.lua
+++ Games/350678807.lua
@@ -1,4 +1,4 @@
 _G.TimGui.Add.G("Door") 
-_G.TimGui.Add.B("BA", "Become amogus", "Door", 1, function() 
+_G.TimGui.Add.B("BA", "Become amogus", "Door", 1,"Стать амогсом", function() 
 game.ReplicatedStorage.Amogus:FireServer() 
 end) 
