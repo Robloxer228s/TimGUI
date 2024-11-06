0d181bfe410ce2bbcdfa2e66a7cffeafd0e8930a
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-11-06 19:59:07 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -9,7 +9,7 @@ if _G.TimGui == nil then
 _G.TimGui = {}
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGUI/main/TimGui.lua"))()
 else
-_G.TimGui.askYN("TimGui is alredy running", "TimGui уже запущен", "Do you want to continue? \n (This may break the script)", "Вы хотите продолжить? \n (Это может сломать скрипт)", function()
+_G.TimGui.askYN("TimGui is alredy running", "Do you want to continue? \n (This may break the script)", "TimGui уже запущен", "Вы хотите продолжить? \n (Это может сломать скрипт)", function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGUI/main/TimGui.lua"))()
 end) 
 end
