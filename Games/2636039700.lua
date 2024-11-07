3ef3f3d3b8199e3265d6f733d92287159abfd263
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-11-08 00:06:51 +0300

    Update 2636039700.lua

--- Games/2636039700.lua
+++ Games/2636039700.lua
@@ -1,9 +1,9 @@
-_G.TimGui.Add.G("UP","Доберись до вершины")
-_G.TimGui.Add.B("UP", "TP to finish", "UP", 1, "ТП в конец", function() 
-local tp = game.Workspace:WaitForChild("TowerSpawn")
-tp = tp:WaitForChild("TowerTop")
-tp = tp:WaitForChild("EndingButton")
-tp = tp:WaitForChild("TipTop")
-tp = tp:WaitForChild("weldo")
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = tp.CFrame
+local up = _G.TimGui.Groups.CreateNewGroup("UP","Доберись до вершины")
+up.Create(1,"UP","TP to finish","ТП в конец",function() 
+	local tp = game.Workspace:WaitForChild("TowerSpawn")
+	tp = tp:WaitForChild("TowerTop")
+	tp = tp:WaitForChild("EndingButton")
+	tp = tp:WaitForChild("TipTop")
+	tp = tp:WaitForChild("weldo")
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = tp.CFrame
 end) 
