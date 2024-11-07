249af8f18817544ad985756abf7b74253ebf6486
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-11-07 22:01:23 +0300

    Update 1318971886.lua

--- Games/1318971886.lua
+++ Games/1318971886.lua
@@ -1,53 +1,41 @@
-_G.TimGui.Add.G("Items", "Предметы") 
-_G.TimGui.Add.G("Game/Lobby", "Игра/лобби") 
-
+-- Break IN
 local Events = game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents")
-local Items = {
-		"Cure",
-		"Apple",
-		"Cookie",
-		"Bloxy Cola",
-		"Chips",
-		"Lollipop",
-		"Epic Pizza",
-		"Pizza 3",
-		"Expired Bloxy Cola",
-		"Teddy Bloxpin",
-		"Bat",
-		"Linked Sword",
-		"Hammer",
-		"Plank", 
-		"Key"
-}
 
-for k, v in Items do
-_G.TimGui.Add.B(k, v, "Items", k, v, function() 
-Events:WaitForChild("GiveTool"):FireServer(tostring(v:gsub(" ", "")))
-end) 
+if Events:FindFirstChild("OutsideRole") then
+	local group = _G.TimGui.Groups.CreateNewGroup("Roles", "Роли") 
+	local costume = group.Create(2,0,"Use costume","Использовать костюм")
+	group.Create(1,1,"The Swat","The Swat",function() 
+	Events.OutsideRole:FireServer("SwatGun",costume.Value)
+	end) 
+	group.Create(1,2,"The Officer","The Officer",function() 
+	Events.OutsideRole:FireServer("Gun",costume.Value)
+	end) 
+	group.Create(1,3,"The Fighter","The Fighter",function() 
+	Events.OutsideRole:FireServer("Sword",costume.Value)
+	end) 
+end	if Events:FindFirstChild("GiveTool") then
+	local group = _G.TimGui.Groups.CreateNewGroup("Items", "Предметы") 
+	local Items = {
+			["Cure"]="Инъекция",
+			["Apple"]="Яблоко",
+			["Cookie"]="Печенье",
+			["Bloxy Cola"]="Блокси кола",
+			["Chips"]="Чипсы",
+			["Lollipop"]="Леденец",
+			["Epic Pizza"]="Эпическая пицца",
+			["Pizza 3"]="Большая пицца",
+			["Expired Bloxy Cola"]="Улучшенная блокси кола",
+			["Teddy Bloxpin"]="Медведка",
+			["Bat"]="Бита",
+			["Linked Sword"]="Меч",
+			["Hammer"]="Молоток",
+			["Plank"]="Доски", 
+			["Key"]="Ключ"
+	}
+
+	for k, v in Items do
+		group.Create(1,k,k,v, function() 
+			Events:WaitForChild("GiveTool"):FireServer(tostring(k:gsub(" ", "")))
+		end) 
+	end
 end
-
-_G.TimGui.Add.B("KE", "Kill bad guys", "Game/Lobby", 1, "Убить плохих челов", function() 
-pcall(function()
-for i, v in pairs(game:GetService("Workspace").BadGuys:GetChildren()) do
-for i = 1, 50 do
-Events:WaitForChild("HitBadguy"):FireServer(v, 10) 
-Events:WaitForChild("HitBadguy"):FireServer(v, 996)
-Events:WaitForChild("HitBadguy"):FireServer(v, 9) 
-Events:WaitForChild("HitBadguy"):FireServer(v, 8) 
-Events:WaitForChild("HitBadguy"):FireServer(v, 996)  
-end
-end
-end)
-end) 
-
-_G.TimGui.Add.B(2, "The Swat(Lobby)", "Game/Lobby", 2, nil, function() 
-game:GetService("ReplicatedStorage").RemoteEvents.OutsideRole:FireServer("SwatGun", false)
-end) 
-
-_G.TimGui.Add.B(3, "The Officer(Lobby)", "Game/Lobby", 3, nil, function() 
-game:GetService("ReplicatedStorage").RemoteEvents.OutsideRole:FireServer("SwatGun", false)
-end) 
-
-_G.TimGui.Add.B(4, "The Fighter(Lobby)", "Game/Lobby", 4, nil, function() 
-game:GetService("ReplicatedStorage").RemoteEvents.OutsideRole:FireServer("SwatGun", false)
-end) 
