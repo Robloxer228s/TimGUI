89fa2a1534777b0855369d39fdb9b1b3b986abc5
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-11-09 01:51:22 +0300

    Update 3898041240.lua

--- Games/3898041240.lua
+++ Games/3898041240.lua
@@ -1,33 +1,35 @@
-_G.TimGui.Add.G("Leader", "Лидер") 
+-- Obby leader
+local group = _G.TimGui.Groups.CreateNewGroup("Leader", "Лидер") 
 local leader = function()
 for k, v in pairs(game.Players:GetChildren()) do
 local hight = v.Character:FindFirstChild("LeaderHighlight")
 if hight then
-if _G.TimGui.SpareFriends then
-if v:IsFriendsWith(game.Players.LocalPlayer.UserId) then return end
-end
+if _G.TimGui.Values.Spare[hight.Parent.Name] then return nil end
 return hight.Parent
+else
+return nil
 end
 end
 end
 
 
-_G.TimGui.Add.B("Touch", "TP to leader", "Leader", 1, "ТП к лидеру", function()
+group.Create(1,"Touch","TP to leader","ТП к лидеру",function()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = leader().HumanoidRootPart.CFrame
 end)
 
-local ATP =  _G.TimGui.Add.CB("AutoTP", "AutoTP to leader", "Leader", 2, "АвтоТП к лидеру")
+local ATP =  group.Create(2,"AutoTP","AutoTP to leader","АвтоТП к лидеру")
 
-local boostEn = _G.TimGui.Add.CB("VisibleBoost", "Set visible for boost", "Leader", 3, "Делать усиление видимым")
+local boostEn = group.Create(2,"VisibleBoost","Set visible for boost","Делать усиление видимым")
 
 while true do
 wait(0.05) 
-pcall(function()
 if ATP.Value then
+local lead = leader()
+if lead then
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = leader().HumanoidRootPart.CFrame
+end
 end 
 if boostEn.Value then
     game.Players.LocalPlayer.PlayerGui.StayOnScreen.Boost.Visible =  true
 end
-end)
 end
