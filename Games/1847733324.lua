f043b93ccccf4d6895291304b4ee200403259c1f
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-11-09 01:31:38 +0300

    Create 1847733324.lua

---
+++ Games/1847733324.lua
@@ -0,0 +1,24 @@
+local function clc(obj,UoD)
+    for k,v in pairs(obj:GetChildren()) do
+        if v.Name == UoD then
+            local detec = v:FindFirstChildOfClass("ClickDetector")
+            if detec then
+                fireclickdetector(detec)
+            end
+        end if v:IsA("Model") or v:IsA("folder") then
+            clc(v,UoD)
+        end
+    end
+end
+local group = _G.TimGui.Groups.CreateNewGroup("Carts","Тележки")
+local carts = game.Workspace.Building.Baseplate["Carts + Jeeps"]
+group.Create(2,"Up","Auto spam for all UP","Авто нажимать на вверх",function(val)
+    while task.wait() and val.Value do
+        clc(carts,"Up")
+    end
+end)
+group.Create(2,"Down","Auto spam for all DOWN","Авто нажимать на вниз",function(val)
+    while task.wait() and val.Value do
+        clc(carts,"Down")
+    end
+end)
