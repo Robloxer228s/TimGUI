b4d01c2f98c178991b63028c283ca62c3bcfbae3
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-11-08 18:33:22 +0300

    Update 2551702779.lua

--- Games/2551702779.lua
+++ Games/2551702779.lua
@@ -1,152 +1,105 @@
-_G.TimGui.Add.G("Zombie lab") 
-local ZN = _G.TimGui.Add.TB("ZN", "Zombie:", "Zombie lab", 6,"Зомби:") 
-local aknz = _G.TimGui.Add.CB("aknz", "Kill NPC Zombies(auto)", "Zombie lab", 9,"Убить НПС зомби(авто)")
-local hn = _G.TimGui.Add.CB("hn", "Hide notifications", "Zombie lab", 10,"Спрятать уведомления")
+local group = _G.TimGui.Groups.CreateNewGroup("Zombie lab") 
+group.Create(1,"virus","Get virus","Получить вирус",function() 
+	game.ReplicatedStorage.Events.GiveVirus:FireServer()
+end) 
+group.Create(1,"cure","Get cure","Получить инъекцию",function() 
+	game.ReplicatedStorage.Events.GiveCure:FireServer()
+end) 
 
+-- Costomize --
 local pathIDK = game.Players.LocalPlayer.PlayerGui.Main
-game.Players.LocalPlayer.CharacterAdded:Connect(function () 
-wait(0.5) 
-pathIDK = game.Players.LocalPlayer.PlayerGui.PlayerGui
-pathIDK.ChildAdded:Connect(function(child) 
-if child.Name == "MoveUp" then
-child.Visible = not hn.Value
-end
-end)
+group.Create(0,"Tittle1","Costomize(Zombie)","Костомизировать зобли")
+group.Create(1,"cf","Change face","Изменить лицо",function() 
+	pathIDK.ChooseFaceFrame.Visible = true
 end) 
-pathIDK.ChildAdded:Connect(function(child) 
-if child.Name == "MoveUp" then
-child.Visible = not hn.Value
-end
+group.Create(1,"cs","Change skin","Изменить цвет кожи",function() 
+	pathIDK.ChooseColorFrame.Visible = true
 end) 
-
-_G.TimGui.Add.B("cf", "Change face", "Zombie lab", 11,"Изменить лицо", function() 
-pathIDK.ChooseFaceFrame.Visible = true
-end) 
-
-_G.TimGui.Add.B("cs", "Change skin", "Zombie lab", 12,"Изменить цвет кожи", function() 
-pathIDK.ChooseColorFrame.Visible = true
-end) 
-
-_G.TimGui.Add.B("virus", "Get virus", "Zombie lab", 1,"Получить вирус", function() 
-game.ReplicatedStorage.Events.GiveVirus:FireServer()
-end) 
-
-_G.TimGui.Add.B("cure", "Get cure", "Zombie lab", 2,"Получить инъекцию", function() 
-game.ReplicatedStorage.Events.GiveCure:FireServer()
-end) 
-
-_G.TimGui.Add.B("dh", "Delete human only", "Zombie lab", 3,"Удалить только для людей", function() 
+-- Other --
+group.Create(0,"Tittle2","Other","Другое")
+group.Create(1,"dh","Delete human only","Удалить только для людей",function() 
 game.Workspace.HumanOnlyDoor:Destroy()
 end) 
-
-_G.TimGui.Add.B("ds", "Delete safe virus zone", "Zombie lab", 4,"Удалить антивирус", function() 
+group.Create(1,"ds","Delete safe virus zone","Удалить антивирус",function() 
 game.Workspace.AntiWeaponZone:Destroy()
 end) 
 
---old
---[[
-local pist = false
-pcall(function() 
-end) 
-if pist then
-return pist
-end
---]]
+group.Create(0,"Tittle3","Kill zombies","Убийство зомби")
+local aknz = group.Create(2,"aknz","Kill NPC Zombies(auto)","Убить НПС зомби(авто)")
 
 local function killZombie(char) 
-local player = game.Players:GetPlayerFromCharacter(char)
--- Check Humanoid
-if not char:FindFirstChild("HumanoidRootPart") then return false end
--- Spare
-if _G.TimGui.SpareFriends and player then
-if player:IsFriendsWith(game.Players.LocalPlayer.UserId) then return true end
-end
-local args = {
-    [1] = char:WaitForChild("Humanoid"),
-    [2] = char:WaitForChild("HumanoidRootPart"),
-    [3] = math.huge,
-}
---In character
-if game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool") then
-if game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool"):FindFirstChild("GunScript_Server") then
-game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool").GunScript_Server.InflictTarget:FireServer(unpack(args))
-return true
-end
-end
--- Find gun and kill char
-for k,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-if v:FindFirstChild("GunScript_Server") then
-v.GunScript_Server.InflictTarget:FireServer(unpack(args))
-return true
-end
-end
-print("You aren't have gun")
-return false
+	if not char:FindFirstChild("HumanoidRootPart") then return false end
+	if _G.TimGui.Values.Spare[char.Name] then return true end
+	local args = {
+		[1] = char:WaitForChild("Humanoid"),
+		[2] = char:WaitForChild("HumanoidRootPart"),
+		[3] = math.huge,
+	}
+	--In character
+	if game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool") then
+		if game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool"):FindFirstChild("GunScript_Server") then
+			game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool").GunScript_Server.InflictTarget:FireServer(unpack(args))
+			return true
+		end
+	end
+	-- Find gun and kill char
+	for k,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+		if v:FindFirstChild("GunScript_Server") then
+			v.GunScript_Server.InflictTarget:FireServer(unpack(args))
+			return true
+		end
+	end
+	return false
 end
 
-_G.TimGui.Add.B("kaz", "Kill All Zombies", "Zombie lab", 5,"Убить всех зомби", function() 
-for i=2,#game.Players:GetPlayers() do
-v=game.Players:GetPlayers()[i]
-killZombie(v.Character)
-end
+group.Create(1,"kaz","Kill All Zombies","Убить всех зомби",function() 
+	for k,v in pairs(game.Players:GetPlayers()) do
+		killZombie(v.Character)
+	end
 end) 
 
-_G.TimGui.Add.B("kz", "Kill Zombie", "Zombie lab", 7,"Убить зомби", function() 
-v = game.Players:FindFirstChild(ZN.Text) 
-killZombie(v.Character)
+local ZN = group.Create(3,"ZN","Zombie:","Зомби:") 
+group.Create(1,"kz","Kill Zombie","Убить зомби",function() 
+	v = game.Players:FindFirstChild(ZN.Value) 
+	if v then
+		killZombie(v.Character)
+	end
 end) 
 
-_G.TimGui.Add.B("knz", "Kill NPC Zombies", "Zombie lab", 8,"Убить НПС зомби", function()
-for k, v in pairs(game.Workspace.Zombies:GetChildren()) do
-killZombie(v)
-end
+group.Create(1,"knz","Kill NPC Zombies","Убить НПС зомби",function()
+	for k, v in pairs(game.Workspace.Zombies:GetChildren()) do
+		killZombie(v)
+	end
 end) 
 
-local round = 8
-local enabled = false
-
-_G.TimGui.Add.CB("KAM","Kill those zombies who are moving","Zombie lab",13,"убивать, тех зомби кто двигается",function(val) 
-enabled = val.Value
-end) 
-
-local PlayersPos = {}
-
+local KTZWAM = group.Create(2,"KAM","Kill those zombies who are moving","убивать, тех зомби кто двигается")
 local function Check(Char) 
-local iiii = Instance.new("BoolValue") 
-iiii.Changed:Connect(function()
-while task.wait(0.25) do
-local pos = Char.HumanoidRootPart.Position
-pos = Vector3.new(math.floor(pos.X / round),math.floor(pos.Y / round), math.floor(pos.Z / round))
-if not (PlayersPos[Char.Name] == pos) then
-if enabled then
-killZombie(Char)
-end
-end
-PlayersPos[Char.Name] = pos
-end
-end) 
-iiii.Value = true
+	Char:WaitForChild("Humanoid"):GetPropertyChangedSignal("MoveDirection"):Connect(function()
+		if KTZWAM.Value then 
+			killZombie(Char)
+		end
+	end)
 end
 
 local function NewPlayer(Player) 
-if not (Player.ClassName == "Player") then return end
-Player.CharacterAdded:Connect(Check)
-Check(Player.Character) 
+	if not Player:IsA("Player") then return end
+	Player.CharacterAdded:Connect(Check)
+	Check(Player.Character) 
 end
 
-for k, v in pairs(game.Players:GetChildren()) do
-NewPlayer(v) 
+for k, v in pairs(game.Players:GetPlayers()) do
+	NewPlayer(v) 
 end
 
 game.Players.PlayerAdded:Connect(NewPlayer)
 
 while true do 
-wait(1) 
-if aknz.Value then
-for k, v in pairs(game.Workspace.Zombies:GetChildren()) do
-if v:FindFirstChild("Humanoid") and v:FindFirstChild("HumanoidRootPart") then
-killZombie(v)
-end 
-end
-end
+	wait(1) 
+	if aknz.Value then
+		for k, v in pairs(game.Workspace.Zombies:GetChildren()) do
+			if v:FindFirstChild("Humanoid") and v:FindFirstChild("HumanoidRootPart") then
+				killZombie(v)
+			end 
+		end
+	end
 end 
