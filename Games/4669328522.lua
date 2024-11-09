7722e0f354097ecf475b562c3e4f8b30fc9c20ca
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-11-09 17:41:49 +0300

    Update 4669328522.lua

--- Games/4669328522.lua
+++ Games/4669328522.lua
@@ -1,10 +1,11 @@
-local ESPM = _G.TimGui.Add.CB("ESPM", "ESP Monsters", "ESP", 3, "ESP на монстров") 
-ESPM.Changed:Connect(function()
+--Zooo
+local group = _G.TImGui.Groups.ESP
+group.Create(2,"ESPM","ESP Monsters""ESP на монстров",function(val)
 for k, chr in pairs(game.Workspace.Maps:GetChildren()) do
 chr = chr:FindFirstChild("Killers") 
 if chr then
 for k, char in pairs(chr:GetChildren()) do
-if ESPM.Value and char.ClassName == "Model" then
+if val.Value and char.ClassName == "Model" then
 local ESP = Instance.new("Highlight")
 ESP.Parent = char
 ESP.Name = "NotEsp"
@@ -26,14 +27,13 @@ end
 end
 end) 
 
-local ESPI = _G.TimGui.Add.CB("ESPI", "ESP Items", "ESP", 4,"ESP На предметы") 
-ESPI.Changed:Connect(function()
+group.Create(2,"ESPI","ESP Items","ESP На предметы",function(val)
 for k, chr in pairs(game.Workspace.Maps:GetChildren()) do
 for k, char in pairs(chr:GetChildren()) do
 if char.Name == "Collections" then
 for k, charr in pairs(char:GetChildren()) do
 if charr.Name == "CollectionProvider" then
-if ESPI.Value and charr.ClassName == "Model" then
+if val.Value and charr.ClassName == "Model" then
 local ESP = Instance.new("Highlight")
 ESP.Parent = charr
 ESP.Name = "NotEsp"
