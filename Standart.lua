fedb00e3c278c134c625db50839e1c561fb13ff7
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-11-24 21:16:36 +0300

    Update Standart.lua

--- Standart.lua
+++ Standart.lua
@@ -1296,3 +1296,207 @@ end)
 Camera.Create(1,"MaxDistance","Max Zoom of camera","Максимальный зум камеры",function()
 	LocalPlayer.CameraMaxZoomDistance = math.huge
 end)
+
+-- Other ------------------------------------------------------------------------------------
+local group = _G.TimGui.Groups.CreateNewGroup("Other","Другое")
+local LocalPlayer = game.Players.LocalPlayer
+
+if game:GetService("UserInputService").TouchEnabled then
+	local Sgui = Instance.new("ScreenGui",LocalPlayer.PlayerGui)
+	local SCenter = Instance.new("ImageLabel",Sgui)
+	local SButton = Instance.new("ImageButton",Sgui)
+	local Enabled = Instance.new("BoolValue",Sgui)
+	local LSize = 32 
+	local LBSizeD = 1.25
+	local OldHumanoid
+
+	Sgui.IgnoreGuiInset = true
+	Sgui.Name = "MouseLock(ShiftLock)"
+    	Sgui.Enabled = false
+	Sgui.ResetOnSpawn = false
+	Sgui.DisplayOrder = 1
+
+	SCenter.Image = "rbxasset://textures/MouseLockedCursor.png"
+	SCenter.Size = UDim2.new(0,LSize,0,LSize)
+	SCenter.Position = UDim2.new(0.5,-LSize/2,0.5,-LSize/2)
+	SCenter.BackgroundTransparency = 1
+	SCenter.Visible = false
+
+	SButton.Image = "rbxasset://textures/ui/mouseLock_off@2x.png"
+	SButton.Size = UDim2.new(0,62/LBSizeD,0,62/LBSizeD)
+	SButton.Position = UDim2.new(0.875,-62/LBSizeD,0.775,-62/LBSizeD)
+	SButton.BackgroundTransparency = 1
+	SButton.Activated:Connect(function() Enabled.Value = not Enabled.Value end)
+
+	Enabled.Name = "Enabled"
+	Enabled.Changed:Connect(function()
+		local char = workspace.CurrentCamera.CameraSubject.Parent
+		LocalPlayer.Character.Humanoid.AutoRotate = not Enabled.Value
+		SCenter.Visible = Enabled.Value
+		if Enabled.Value then
+			SButton.Image = "rbxasset://textures/ui/mouseLock_on@2x.png"
+			if char:FindFirstChild("Humanoid") then
+				oldHumanoid = char.Humanoid
+				char.Humanoid.CameraOffset += Vector3.new(2.5,0,0)
+			end
+		else
+			SButton.Image = "rbxasset://textures/ui/mouseLock_off@2x.png"
+			if char:FindFirstChild("Humanoid") then
+				if oldHumanoid then
+					oldHumanoid.CameraOffset += Vector3.new(-2.5,0,0)
+				end
+			end
+		end
+	end)
+
+	local function upd()
+		if Enabled.Value then
+			local camera = workspace.CurrentCamera
+			local Char = camera.CameraSubject.Parent
+			if Char:IsA("Model") then
+				local HRP = Char.PrimaryPart
+				if HRP then
+					local Length = 900000
+					local CamR = Vector3.new(camera.CFrame.LookVector.X *Length, HRP.Position.Y, camera.CFrame.LookVector.Z*Length)
+					HRP.CFrame = CFrame.new(HRP.Position, CamR)
+				end
+			end
+		end
+	end
+
+	LocalPlayer:GetMouse().Move:Connect(upd)
+	game:GetService("RunService").RenderStepped:Connect(upd)
+    	workspace.CurrentCamera:GetPropertyChangedSignal("CameraSubject"):Connect(function(char)
+		local char = workspace.CurrentCamera.CameraSubject.Parent
+		if char:IsA("Model") then
+			local en = Enabled.Value
+		        Enabled.Value = false
+		        char:WaitForChild("HumanoidRootPart",math.huge)
+		        wait()
+		        Enabled.Value = en
+		end
+    	end)
+	    group.Create(2,"SL(ML)","Shift Lock/Mouse Lock(for mobile)","Шифт лок/блокировка мыши (для телефонов)",function(val)
+	        Sgui.Enabled = val.Value
+	    end)
+else
+    if mouse1click ~= nil then
+        group.Create(2,"Clicker","AutoClicker","Автокликер",function(val)
+            wait(1)
+            while task.wait() and val.Value do
+                mouse1click()
+            end
+        end)
+    end
+    group.Create(1,"SL","Enable shift Lock","Включить shiftlock switch",function()
+        LocalPlayer.DevEnableMouseLock = true
+    end)
+end
+
+group.Create(0,"Try hack game","Try hack game","Попытки взлома игры")
+local function checkPass(obj,ParentName)
+    if (string.find(obj.Name,"pass") ~= nil) or (string.find(obj.Name,"Pass") ~= nil) then
+        return true
+    end
+    if ParentName then
+        return (string.find(obj.Parent.Name,"pass") ~= nil) or (string.find(obj.Parent.Name,"Pass") ~= nil)
+    end
+end
+
+group.Create(1,"Attempt Get Passes","Attempt Get All Passes","Попытаться получить все геймпассы",function()
+    local Attempt = 0
+    local function up(val)
+        for _,v in pairs(val:GetChildren()) do
+            if v:IsA("BoolValue") then
+                if checkPass(v,true) then
+                    v.Value = not v.Value
+                    Attempt += 1
+                end
+            end
+            up(v)
+        end
+    end
+    up(LocalPlayer)
+    if Attempt == 0 then
+        _G.TimGui.Print("Gamepasses","Not found","Gamepasses","Не найдено")
+    else
+        _G.TimGui.Print("Gamepasses",Attempt.." has been activated","Gamepasses","Активировано: "..Attempt)
+    end
+end)
+
+group.Create(1,"Attempt Get PassesGUI","Attempt Make Visible All Gui(for pass)","Попытаться открыть все окна (с пассами)",function()
+    local Attempt = 0
+    local function up(val)
+        for _,v in pairs(val:GetChildren()) do
+            if checkPass(v,false) then
+                if v:IsA("LayerCollector") then
+                    v.Enabled = not v.Enabled
+                    Attempt += 1
+                elseif v:IsA("GuiObject") then
+                    v.Visible = not v.Visible
+                    Attempt += 1
+                end
+            end
+            up(v)
+        end
+    end
+    up(LocalPlayer)
+    if Attempt == 0 then
+        _G.TimGui.Print("Gamepasses","Not found","Gamepasses","Не найдено")
+    else
+        _G.TimGui.Print("Gamepasses",Attempt.." has been activated","Gamepasses","Активировано: "..Attempt)
+    end
+end)
+
+group.Create(1,"Attempt activate all privilegies","Attempt activate all privilegies?","Попытаться получить все привилегии?",function()
+    local Attempt = 0
+    local function up(val)
+        for _,v in pairs(val:GetChildren()) do
+            if v:IsA("BoolValue") then
+                if not v.Value then
+                    Attempt += 1
+                end
+                v.Value = true
+            end
+            up(v)
+        end
+    end
+    up(LocalPlayer)
+    if Attempt == 0 then
+        _G.TimGui.Print("Privilegies?","Not found","Privilegies?","Не найдено")
+    else
+        _G.TimGui.Print("Privilegies?",Attempt.." has been activated","Привилегии?","Активировано: "..Attempt)
+    end
+end)
+
+local HideGui = group.Create(2,"Hide All Guis","Hide All Guis","Спрятать весь интерфейс")
+local Hided = {}
+local function UpdHideGui(obj,hide)
+    if hide then
+        Hided[obj] = obj.Enabled
+        obj.Enabled = false
+    elseif Hided[obj] then
+        obj.Enabled = Hided[obj]
+    end
+end
+local function NewGui(gui)
+    if gui:IsA("LayerCollector") then
+        gui:GetPropertyChangedSignal("Enabled"):Connect(function()
+            if gui.Enabled then
+                UpdHideGui(gui,HideGui.Value)
+            end
+        end)
+        UpdHideGui(gui,HideGui.Value)
+    end
+end
+LocalPlayer.PlayerGui.ChildAdded:Connect(NewGui)
+for k,v in pairs(LocalPlayer.PlayerGui:GetChildren()) do
+    NewGui(v)
+end
+HideGui.OnChange(function(val)
+    for k,v in pairs(LocalPlayer.PlayerGui:GetChildren()) do
+        if v:IsA("LayerCollector") then
+            UpdHideGui(v,val.Value)
+        end
+    end
+end)
