e268b2a2b6576cddc1afdd8db894573d941780bb
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-11-29 16:05:14 +0300

    Update 2327642508.lua

--- Games/2327642508.lua
+++ Games/2327642508.lua
@@ -1,20 +1,27 @@
 local group = _G.TimGui.Groups.CreateNewGroup("Laundry","Прачечная")
 local LocalPlayer = game.Players.LocalPlayer
 local Clothing = game.Workspace.Debris.Clothing
+local Events = game.ReplicatedStorage.Events
 group.Create(2,"Speed","Donate: NitroSpeed","НитроСкорость",function(val)
     LocalPlayer.Gamepasses.NitroSpeed.Value = val.Value
 end)
 group.Create(0,"Tittle","Grab","Брать")
 group.Create(0,"TIP","P.S:It grab clothings in 25 studs","P.S:Оно берёт одежду менее 25 шагов")
-local function TPClothing(notrare)
+local function TPClothing(notrare,fullbaskNR)
     for k,v in pairs(Clothing:GetChildren()) do
         if v.Name == "Magnet" then continue end
         if v:FindFirstChild("Sound") or notrare then
-            local magn = math.abs(v.CFrame.Position.Magnitude - LocalPlayer.Character.PrimaryPart.Position.Magnitude)
-            if magn > 25 then continue end
-            v.Anchored = true
-            v.CFrame = LocalPlayer.Character.PrimaryPart.CFrame
-            if notrare then return end
+            if LocalPlayer.NonSaveVars.TotalWashingMachineCapacity.Value < LocalPlayer.NonSaveVars.BackpackAmount.Value + 1 then
+            	_G.TimGui.Print("Warning","You don't have enough washing machine capacity","Проблема","Тебе не хватает место в машинах")
+            elseif 0 < LocalPlayer.NonSaveVars.BackpackAmount.Value and LocalPlayer.NonSaveVars.BasketStatus.Value == "Clean" then
+            	_G.TimGui.Print("Warning","You can't put dirty laundry in a clean basket","Проблема","Ты не можешь положить это в корзину с чистыми панталонами")
+            elseif LocalPlayer_upvr_2.NonSaveVars.BackpackAmount.Value < LocalPlayer_upvr_2.NonSaveVars.BasketSize.Value then
+            	_G.TimGui.Print("Warning","Basket is full","Проблема","Корзина пуста")
+            else
+            	game.SoundService.Misc.Pick:Play()
+            	Events.GrabClothing:FireServer(v)
+            end
+            if notrare and not fullbaskNR then return end
         end
     end
 end
@@ -22,10 +29,9 @@ end
 group.Create(1,"TPMagnit","Grab Magnit","Взять магнит",function()
     for k,v in pairs(Clothing:GetChildren()) do
         if v.Name == "Magnet" then
-            local magn = math.abs(v.CFrame.Position.Magnitude - LocalPlayer.Character.PrimaryPart.Position.Magnitude)
-            if magn > 25 then continue end
-            v.Anchored = true
-            v.CFrame = LocalPlayer.Character.PrimaryPart.CFrame
+            game.SoundService.Misc.Pick:Play()
+            Events.GrabClothing:FireServer(v)
+            break
         end
     end
 end)
@@ -35,7 +41,11 @@ group.Create(1,"TPRareClothings","Grab Rare Clothings","Взять редкие 
 end)
 
 group.Create(1,"TPClothings","Grab Clothing","Взять вещь",function()
-    TPClothing(true)
+    TPClothing(true,false)
+end)
+
+group.Create(1,"TPAllClothings","Grab All Clothings","Взять все вещи",function()
+    TPClothing(true,true)
 end)
 
 group.Create(0,"Tittle2","TP","ТП")
