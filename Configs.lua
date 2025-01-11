3bf4e8b87fb343525bcbc29291ab876a06f42138
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2025-01-11 15:39:17 +0300

    Update Configs.lua

--- Configs.lua
+++ Configs.lua
@@ -2,6 +2,7 @@ local HttpService = game:GetService("HttpService")
 if _G.TimGui.Saves.Enabled then
 	local group = _G.TimGui.Groups.CreateNewGroup("Configs")
 	local path = "TimGui/Configs/"
+	local pathKeys = "TimGui/KeybindsConfigs/"
 	local loadedCFG = {}
 	local selected
 	local lastCFG = _G.TimGui.Saves.Load("Config")
@@ -29,6 +30,7 @@ if _G.TimGui.Saves.Enabled then
 		end
 	end
 	makefolder(path)
+	makefolder(pathKeys)
 	local name = group.Create(3,1,"Name:","Имя:",function(val)
 		local result = string.gsub(val.Main.Text,"/","")
 		if result ~= val.Main.Text then
@@ -37,6 +39,7 @@ if _G.TimGui.Saves.Enabled then
 	end)
 	group.Create(1,2,"Create empty","Создать пустой",function()
 		writefile(path..name.Value,"{}")
+		writefile(pathKeys..name.Value,"{}")
 		CreateCFGButton(path..name.Value)
 	end)
 	group.Create(0,3,"Manage configs","Управление конфигами")
@@ -49,7 +52,7 @@ if _G.TimGui.Saves.Enabled then
 	end)
 	group.Create(1,5,"Save","Сохранить",function()
 		if selected ~= nil then
-			local save = HttpService:JSONDecode(readfile(path..selected))
+			local save = HttpService:JSONDecode(readfile(path..selected)) or {}
 			for gn,g in pairs(_G.TimGui.Groups) do
 				if type(g) == "table" and gn ~= groups then
 					local groupsave = save[gn] or {}
@@ -68,6 +71,17 @@ if _G.TimGui.Saves.Enabled then
 				end
 			end
 			writefile(path..selected,HttpService:JSONEncode(save))
+			local keysSave = HttpService:JSONDecode(readfile(pathKeys..selected)) or {}
+			for k,v in pairs(_G.TimGui.Keybinds.GetTable()) do
+				local key = {}
+				for nm,_ in pairs(v) do
+					table.insert(key,nm)
+				end
+				keysSave[k] = key
+			end
+			writefile(pathKeys..selected,HttpService:JSONEncode(keysSave))
+		else
+			_G.TimGui.Print("Configs","Config not selected","Конфигурации","Не выбрано")
 		end
 	end)
 	local loadCFG = group.Create(1,6,"Load","Загрузить",function()
@@ -89,6 +103,21 @@ if _G.TimGui.Saves.Enabled then
 					end
 				end
 			end
+			local keysSave = HttpService:JSONDecode(readfile(pathKeys..selected)) or {}
+			for key,v in pairs(keysSave) do
+				for _,name in pairs(v) do
+					local split = string.split(name,".")
+					local group = _G.TimGui.Groups[split[1]]
+					if group then
+						local obj = group.Objects[split[2]]
+						if obj then
+							_G.TimGui.Keybinds.Set(obj,key)
+						end
+					end
+				end
+			end
+		else
+			_G.TimGui.Print("Configs","Config not selected","Конфигурации","Не выбрано")
 		end
 	end)
 	group.Create(0,7,"Configs","Конфигурации")
