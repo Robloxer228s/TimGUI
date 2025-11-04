9db0ee8ea065d5b7b06d458b6ce8125cdf89b1f4
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2025-11-04 19:23:55 +0300

    Mini update for reset + debug

--- Main.lua
+++ Main.lua
@@ -5,11 +5,18 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGUI/m
 
 ]]
 
-if _G.TimGui == nil then
+if type(_G.TimGui)~="table" then
 _G.TimGui = {}
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGUI/main/TimGui.lua"))()
-else
-_G.TimGui.askYN("TimGui is alredy running", "Do you want to continue? \n (This may break the script)", "TimGui уже запущен", "Вы хотите продолжить? \n (Это может сломать скрипт)", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGUI/main/TimGui.lua"))()
-end) 
+else local function Open()
+    if _G.TimGui.Configs and _G.TimGui.Configs.ResetToDefault then
+      _G.TimGui.Configs.ResetToDefault()
+    end loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGUI/main/TimGui.lua"))()
+  end
+  if _G.TimGui.Modules and _G.TimGui.Modules.askYN then
+    _G.TimGui.Modules.askYN("TimGui is alredy running", "Do you want to continue? \n (This may break the script)", "TimGui уже запущен", "Вы хотите продолжить? \n (Это может сломать скрипт)", function()
+      Open()
+    end) 
+  else Open()
+  end
 end
