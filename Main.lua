ad3efb2ee7d9ca8452b98922bc094e4cb3545b53
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2025-11-04 21:34:57 +0300

    Wtf

--- Main.lua
+++ Main.lua
@@ -4,19 +4,17 @@ Please, use this script(for updates):
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGUI/main/Main.lua"))()
 
 ]]
-
-if type(_G.TimGui)~="table" then
-_G.TimGui = {}
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGUI/main/TimGui.lua"))()
-else local function Open()
-    if _G.TimGui.Configs and _G.TimGui.Configs.ResetToDefault then
+local function Open()
+    if _G.TimGui and _G.TimGui.Configs and _G.TimGui.Configs.ResetToDefault then
       _G.TimGui.Configs.ResetToDefault()
     end loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/TimGUI/main/TimGui.lua"))()
-  end
+end
+if type(_G.TimGui)~="table" then
+    _G.TimGui = {}
+    Open()
+else 
   if _G.TimGui.Modules and _G.TimGui.Modules.askYN then
-    _G.TimGui.Modules.askYN("TimGui is alredy running", "Do you want to continue? \n (This may break the script)", "TimGui уже запущен", "Вы хотите продолжить? \n (Это может сломать скрипт)", function()
-      Open()
-    end) 
+    _G.TimGui.Modules.askYN("TimGui is alredy running", "Do you want to continue? \n (This may break the script)", "TimGui уже запущен", "Вы хотите продолжить? \n (Это может сломать скрипт)", Open) 
   else Open()
   end
 end
