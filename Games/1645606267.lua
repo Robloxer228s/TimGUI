445001f22805bb7e9715704bdad67405e07259c7
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2025-11-16 22:11:24 +0300

    Update 1645606267.lua

--- Games/1645606267.lua
+++ Games/1645606267.lua
@@ -4,7 +4,7 @@ local ESPV = group.Create(2,"ESPVgame","ESP all","ESP все",function()_G.TimGu
 local ESPF = group.Create(2,"ESPFgame","ESP froggy","ESP к лягушке",function()_G.TimGui.Modules.ESP.Refresh()end) 
 
 _G.TimGui.Modules.ESP.Bind(2,function(Player)
-	if Player.Character.Name=="Frogge"then
+	if Player.Character.Name~=Player.Name then
 		if ESPF.Value then
 			return Color3.new(1,0,0)
 		end
