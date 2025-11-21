9db685bd93e327c42822423af56819543bffdd6e
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2025-11-21 21:49:48 +0300

    Update 703124385.lua

--- Games/703124385.lua
+++ Games/703124385.lua
@@ -1,20 +1,24 @@
 local LocalPlayer = game.Players.LocalPlayer
 local group = _G.TimGui.Groups.CreateNewGroup("Tower of HELL","Башня ада :>")
-
+group.Create(0,"Warn","Warning: You can get banned for standard functions, experiment on 2 account","Предупреждение:за стандартные функции тебя могут забанить, эксперементируй на 2 акке")
 local Speed = group.Create(3,"SpeedT","Speed:","Скорость:")
 group.Create(1,"Set speed","Set speed","Установить скорость",function()
-    game.ReplicatedStorage.globalSpeed.Value = Speed.Value
+    game.ReplicatedStorage.GameValues.globalSpeed.Value = Speed.Value
 end)
 
 local NoKill = group.Create(2,"NoKill","Disable kill bricks","Выключить убивашки",function(val)
-    LocalPlayer.Character.KillScript.Disabled = val.Value
+    game.ReplicatedStorage.GameValues.killbricksDisabled.Value = val.Value
+end) game.ReplicatedStorage.GameValues.killbricksDisabled.Changed:Connect(function()
+    if not game.ReplicatedStorage.GameValues.killbricksDisabled.Value then
+        game.ReplicatedStorage.GameValues.killbricksDisabled.Value = NoKill.Value    
+    end
 end)
 LocalPlayer.CharacterAdded:Connect(function()
     LocalPlayer.Character:WaitForChild("KillScript").Disabled = NoKill.Value
 end)
 
 group.Create(1,"Infinity jump","Infinity jump","Бесконечный прыжок",function()
-    game.ReplicatedStorage.globalJumps.Value = 10000000000
+    game.ReplicatedStorage.GameValues.globalJumps.Value = 10000000000
 end)
 
 group.Create(1,"FTT","Finish the tower","Завершить башню",function()
@@ -22,7 +26,7 @@ group.Create(1,"FTT","Finish the tower","Завершить башню",function
 end)
 
 group.Create(1,"OffBunny","Off bunny hop","Выключить банихоп",function()
-    game.ReplicatedStorage.bunnyJumping.Value = false
+    game.ReplicatedStorage.GameValues.bunnyJumping.Value = false
 end)
 
 local NotAC = _G.TimGui.Groups.Map.Objects
@@ -46,11 +50,10 @@ NotAC.SettingsWF.Destroy()
 NotAC.WalkFling.Destroy()
 NotAC.FlyNMS.Destroy()
 NotAC.FlyNM.Destroy()
-NotAC["Allow Sit on fly v2"].Visible = false
 _G.TimGui.Groups.ACGroup.Objects.FlyUPS.Main.Changed:Connect(function()
     _G.TimGui.Groups.ACGroup.Objects.FlyUPS.Main.Value = false
 end)
-_G.TimGui.Groups["Freeze players"].Visible = false
+_G.TimGui.Groups["Freeze"].Visible = false
 local NotAC = _G.TimGui.Groups.FUN.Objects
 NotAC.HappyModSwim.Visible = false
 NotAC.Fling.Value = false
