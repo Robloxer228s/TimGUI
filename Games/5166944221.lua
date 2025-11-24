ef91a77900d1f323d7d503179edf15d0ea1b63f8
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2025-11-24 21:05:44 +0300

    Update 5166944221.lua

--- Games/5166944221.lua
+++ Games/5166944221.lua
@@ -14,7 +14,7 @@ local function AddToFilter(Inst)
 end _G.TimGui.Modules.Players.ForEveryone(function(Player)
     Player.CharacterAdded:Connect(AddToFilter)
     if Player.Character then AddToFilter(Player.Character) end
-end,false)
+end,true)
 group.Create(3,"AutoParrySpeedMulty","AutoParry distance multy:","Множитель дистанции для отбива:",function(val)
     if tonumber(val.Value) then SpeedMulty=tonumber(val.Value) end
 end).Main.Text = SpeedMulty
@@ -69,55 +69,57 @@ end) group.Create(1,"NoobServer","Noob server","НУБ сервер",function()
     _G.TimGui.Modules.Servers.JoinToPlace(83678792452277)
 end) group.Create(1,"TradeServer","Trade server","Трейд сервер",function()
     _G.TimGui.Modules.Servers.JoinToPlace(119260352090770)
-end)
+end) local BallPos
 while task.wait() do
     if not BallShadow or not Ball then
         BallShadow = game.Workspace.FX:WaitForChild("BallShadow",math.huge)
         Ball = game.Workspace:WaitForChild("Part",math.huge)
-        AddToFilter(Ball)
+		Ball:GetPropertyChangedSignal("Position"):Connect(function()
+			BallPos = Ball.Position
+		end) AddToFilter(Ball)
         LastBallPos = nil
         LastYPos = nil
     elseif not BallShadow.Parent or not Ball.Parent then
         Ball,BallShadow = nil,nil
-    else local BallPos = BallShadow.Position
+    elseif BallPos then --local BallPos = BallShadow.Position
         if not LastBallPos then LastBallPos = BallPos end
         local moveDir = (LastBallPos-BallPos)
-        local speed = (moveDir.Magnitude-math.abs(moveDir.Y)+0.5)*SpeedMulty
+        local speed = (moveDir.Magnitude+0.5)*SpeedMulty--math.abs(moveDir.Y)
         if AutoParry.Value and Ball.Highlight.FillColor ~= WhiteB then
             local HRP = LP.Character.PrimaryPart
-            local distance = (HRP.Position-BallPos).Magnitude-math.abs(HRP.Position.Y-BallPos.Y)
+            local distance = (HRP.Position-BallPos).Magnitude--math.abs(HRP.Position.Y-BallPos.Y)
             local skip = false
             if TryToAnticipate.Value and moveDir~=Vector3.zero then
                 if game.Workspace:Raycast(BallPos,moveDir*SpeedMulty,RaycastParam) then
                     print("SPEED/2")
                     speed = speed/2
                 end
-            end local y = (BallShadow.Decal.Transparency-0.3)*400
+            end --local y = (BallShadow.Decal.Transparency-0.3)*400
             if not skip and distance<speed then
                 if CheckRaycast(HRP.Position,BallPos) then
-                    local yMagn = math.abs((BallPos.Y+y)-HRP.Position.Y)
-                    if not LastYPos then LastYPos = y end
-                    if yMagn<(speed+math.abs(LastYPos-y)*SpeedMulty)+5 then
-                        if TryToAnticipate.Value and moveDir~=Vector3.zero then
-                            local MagnToChar = (BallPos-HRP.Position).Magnitude
-                            local AddingPos = (BallPos+moveDir.Unit*MagnToChar*1.5)-HRP.Position
-                            local NOAP = (BallPos-HRP.Position)
-                            local MultyRes = NOAP.X+NOAP.Z
-                            if MultyRes>0 then MultyRes = 1 else MultyRes = -1 end
-                            local Res = (AddingPos.X+AddingPos.Z)*MultyRes
-                            if Res>0 then
-                                Parry()
-                            else print("Parry blocked, resV:",Res)
-                                print("AddingPosV:",AddingPos)
-                            end
-                        else Parry()
-                        end
-                    else print("YMagnitude blocked, ymagn:",yMagn)
-                    end
+                    --local yMagn = math.abs((BallPos.Y+y)-HRP.Position.Y)
+                    --if not LastYPos then LastYPos = y end
+                    --if yMagn<(speed+math.abs(LastYPos-y)*SpeedMulty)+5 then
+					if TryToAnticipate.Value and moveDir~=Vector3.zero then
+						local MagnToChar = (BallPos-HRP.Position).Magnitude
+						local AddingPos = (BallPos+moveDir.Unit*MagnToChar*1.5)-HRP.Position
+						local NOAP = (BallPos-HRP.Position)
+						local MultyRes = NOAP.X+NOAP.Z
+						if MultyRes>0 then MultyRes = 1 else MultyRes = -1 end
+						local Res = (AddingPos.X+AddingPos.Z)*MultyRes
+						if Res>0 then
+							Parry()
+						else print("Parry blocked, resV:",Res)
+							print("AddingPosV:",AddingPos)
+						end
+					else Parry()
+					end
+                    --else print("YMagnitude blocked, ymagn:",yMagn)
+                    --end
                 else print("Parry blocked!")
                 end
             end
-        end LastYPos = y
+        end --LastYPos = y
         LastBallPos = BallPos
     end 
 end
